3345b8822ab39474fb9940fefccca7f3dd5f17f6
shrac1708 <shravanic888@gmail.com>
2026-04-05 19:27:12 +0530

    Fix model save: use .keras format to avoid _DictWrapper bug on Python 3.12+

--- api/main.py
+++ api/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MODEL = tf.keras.models.load_model("../potatoes.h5", compile=False)
+MODEL = tf.keras.models.load_model("../potatoes.keras", compile=False)
 
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
